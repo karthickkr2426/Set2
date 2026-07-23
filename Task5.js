d5e31f76b4ad7a371484ee7c1c6293e696eb346f
karthickkr2426 <karthickkr.2426@gmail.com>
2026-07-23 09:46:20 +0530

    Create Task5.js

---
+++ Task5.js
@@ -0,0 +1,14 @@
+// Task 5: Hospital Patient System — Classify a Record
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("Patient is an Adult.");
+} else {
+    console.log("Patient is a Minor.");
+}
+
+/*
+Expected Output:
+Patient is an Adult.
+*/
