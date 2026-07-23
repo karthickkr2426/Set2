c41b52d7ef823a28abb15b44b71df514fe2edeb0
karthickkr2426 <karthickkr.2426@gmail.com>
2026-07-23 09:45:56 +0530

    Create Task4.js

---
+++ Task4.js
@@ -0,0 +1,13 @@
+// Task 4: Hospital Patient System — Perform a Calculation
+
+let patient1Age = 45;
+let patient2Age = 30;
+
+let ageDifference = patient1Age - patient2Age;
+
+console.log("Age Difference:", ageDifference);
+
+/*
+Expected Output:
+Age Difference: 15
+*/
