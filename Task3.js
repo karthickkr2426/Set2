d07e1f87d64442e33dbb4b719363c7d24f6babdb
karthickkr2426 <karthickkr.2426@gmail.com>
2026-07-23 09:45:35 +0530

    Create Task3.js

---
+++ Task3.js
@@ -0,0 +1,16 @@
+// Task 3: Hospital Patient System — Identify Datatypes
+
+let patientAge = 25;
+let patientName = "Rahul";
+let isAdmitted = true;
+
+console.log(typeof patientAge);
+console.log(typeof patientName);
+console.log(typeof isAdmitted);
+
+/*
+Expected Output:
+number
+string
+boolean
+*/
