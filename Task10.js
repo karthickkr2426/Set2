596cc8d729a2f9afef9dd514a9f146ce5324ad78
karthickkr2426 <karthickkr.2426@gmail.com>
2026-07-23 09:48:03 +0530

    Create Task10.js

---
+++ Task10.js
@@ -0,0 +1,24 @@
+// Task 10: Hospital Patient System — Create and Print an Object
+
+let patient = {
+    id: 101,
+    name: "Ramesh",
+    age: 35,
+    gender: "Male",
+    disease: "Typhoid"
+};
+
+console.log(patient.id);
+console.log(patient.name);
+console.log(patient.age);
+console.log(patient.gender);
+console.log(patient.disease);
+
+/*
+Expected Output:
+101
+Ramesh
+35
+Male
+Typhoid
+*/
